aefb0d472253d6fb7e0d79b668df7cbc4a5e0447
Tucker Eck <115662454+tuckereck@users.noreply.github.com>
2024-12-19 22:11:53 -0500

    Release

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/root/playstation/kill.sh
+/root/playstation/pppwn_arch64 --interface br-lan --fw 1100 --stage1 /root/playstation/stage1_1100.bin --stage2 /root/playstation/stage2_1100.bin --auto-retry
